507c25b60792a77a8460147fb7fde6e874492a2c
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-04-18 10:29:42 -0700

    docker container built from scratch using fork of andrew-d static-binary script for nmap

--- build.sh
+++ build.sh
@@ -2,10 +2,22 @@
 
 # save start dir
 startDir=$(pwd)
+mkdir -p build/{bin,tmp}
+
+cd nmap-build
+docker build -t static-binaries-nmap .
+docker run -v "${startDir}/build/bin":/output static-binaries-nmap
+
+if [[ "${?}" != 0 ]]; then
+    echo "Nmap compilation failed"
+    exit 1
+fi
+
+cd "${startDir}"
 
 cd build
 echo -n "Compiling go binary..."
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main ..
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bin/main ..
 echo "Done"
-docker build . -t gomapper
+docker build . -t gomapper --no-cache
 cd "${startDir}"
